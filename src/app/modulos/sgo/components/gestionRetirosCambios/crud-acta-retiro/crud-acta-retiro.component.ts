44198e72e08a1d56b8e7e4c13ea401c9eebd8874
Benjamin <benjamin.salazar17@gmail.com>
2019-04-04 18:30:57 -0300

    first

--- src/app/modulos/sgo/components/gestionRetirosCambios/crud-acta-retiro/crud-acta-retiro.component.ts
+++ src/app/modulos/sgo/components/gestionRetirosCambios/crud-acta-retiro/crud-acta-retiro.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, AfterViewInit, ViewChild, Renderer,Inject } from '@angular/core';
+import { DataTableDirective } from 'angular-datatables';
+import { Subject } from 'rxjs';
+import { TableConfigSgo } from '../../../util/tableConfigSgo.util';
+
 
 @Component({
   selector: 'app-crud-acta-retiro',
   templateUrl: './crud-acta-retiro.component.html',
   styleUrls: ['./crud-acta-retiro.component.scss']
 })
-export class CrudActaRetiroComponent implements OnInit {
+export class CrudActaRetiroComponent implements OnInit, OnDestroy, AfterViewInit {
 
-  constructor() { }
+  @ViewChild(DataTableDirective)
+  dtElement: DataTableDirective;
+
+  @ViewChild('myModalEmailInfo') myModalEmailInfo;
+  dtOptions: any;
+  dtTrigger:Subject<any> = new Subject();
+
+  constructor( public configTable: TableConfigSgo){
+    this.dtOptions = this.configTable.dtOptionsExport.buttons = [];
+    this.dtOptions = this.configTable.dtOptionsExport.searching = false;
+    
+    
+  }
 
   ngOnInit() {
+    //hacemos un arr[] de dtOptions ya que tenemos mas de una tabla en nuestro componente
+    this.dtOptions = this.configTable.dtOptionsExport;
   }
+
+  ngAfterViewInit(): void {
+    this.dtTrigger.next();
+  }
+
+  ngOnDestroy(): void {
+    // Do not forget to unsubscribe the event
+    this.dtTrigger.unsubscribe();
+  }
+
+  refrescarTable() {
+    this.dtTrigger.next();
+    this.dtElement.dtInstance.then((dtInstance: DataTables.Api) => {
+      dtInstance.destroy();
+      this.dtTrigger.next();
+    });
+  }
+
+  
+
+  
+
 
 }
